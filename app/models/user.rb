6b8c5fb212d8c323e0b56af5479c1a24b0b883ec
Joel Lusky <joelzlusky@gmail.com>
2014-09-23 13:08:12 -0400

    user valadations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates :phone_number, presence: true
+	validates :phone_number, length: { is: 10 }
+	validates :phone_number, :numericality => {:only_integer => true}
+	validates :name, presence: true
 end
