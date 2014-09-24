27cfd0a8e990c316451a39adfa3f3b80d91400b0
alfonsopintos <alfonsopintos@gmail.com>
2014-09-23 23:04:13 -0400

    connection to server established

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,26 +1,51 @@
 class Project < ActiveRecord::Base
-	belongs_to :user
+	require 'json'
+
 	after_validation :send_rabbit 
 
+	belongs_to :user
 	
 	def send_rabbit
 		if self.status == "Funding" 
-		    conn = Bunny.new
-		    conn.start
+		    connection = Bunny.new(
+		    	:host => "tiger.cloudamqp.com", 
+		    	:vhost => "jnrczvil", 
+		    	:user => "jnrczvil", 
+		    	:password => "sIMlgcE-xekl1Fo5hEQEIApzaBtGP8tO")
+		    connection.start
 
-		    ch = conn.create_channel
-		    q = ch.queue("bunny.examples.hello_world", :auto_delete => true)
-		    x = ch.default_exchange
+		    channel = connection.create_channel
+		    queue = channel.queue("bunny", :auto_delete => true)
+		    exchange = channel.default_exchange
 
-		    q.subscribe do |delivery_info, metadata, payload|
+		    queue.subscribe do |delivery_info, metadata, payload|
 		      puts "Recieved #{payload}"
+		      puts "delivery info: #{delivery_info}"
+		      puts "metadata: #{metadata}"
+
 		    end
 
-		    x.publish("Hello!", :routing_key => q.name)
+		    exchange.publish(hash, :routing_key => queue.name)
 
 		    sleep 1.0
-	    	conn.close
+	    	connection.close
 		end
   	end
 
+  	def hash
+  	{'header' => { 
+		ref_id: 'random number', 
+		client: 'es_web', 
+		timestamp: Time.now, 
+		priority: 'normal', 
+		auth_token: '?', 
+		event_type: 'project_status_update'},
+	'body' => {
+		user_id: self.user_id, 
+		channel: "email", 
+		user_email: self.user.email, 
+		username: self.user.name, 
+		user_mobile: '73649283'}
+	}.to_json
+	end
 end
