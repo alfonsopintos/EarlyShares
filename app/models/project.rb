eb8310d4bbd9f218c0b1d74d63e4b3acc65b176f
alfonsopintos <alfonsopintos@gmail.com>
2014-09-23 21:45:38 -0400

    send_rabbit working when status changess to funding

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,26 @@
 class Project < ActiveRecord::Base
 	belongs_to :user
+	after_validation :send_rabbit 
+
+	
+	def send_rabbit
+		if self.status == "Funding" 
+		    conn = Bunny.new
+		    conn.start
+
+		    ch = conn.create_channel
+		    q = ch.queue("bunny.examples.hello_world", :auto_delete => true)
+		    x = ch.default_exchange
+
+		    q.subscribe do |delivery_info, metadata, payload|
+		      puts "Recieved #{payload}"
+		    end
+
+		    x.publish("Hello!", :routing_key => q.name)
+
+		    sleep 1.0
+	    	conn.close
+		end
+  	end
+
 end
