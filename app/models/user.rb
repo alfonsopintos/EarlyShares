e077e8ecbca9cb4063a49548e688088c403fec89
Joel Lusky <joelzlusky@gmail.com>
2014-09-24 12:30:40 -0400

    user model valadations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :name, presence: true
 	validates_uniqueness_of :email
+	validates :email, presence: true
+	validates :cell_phone, presence: true
+	validates :cell_phone, length: { is: 10 }
+	validates :cell_phone, :numericality => {:only_integer => true}
 
 	has_many :projects
 
